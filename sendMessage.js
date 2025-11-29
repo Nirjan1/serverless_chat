56bbe26c71a44ed94033745907444f17fdf8d02e
Nirjan Mondal <mondalnirjan568@gmail.com>
2025-11-29 11:31:48 +0530

    Update sendMessage.js

--- sendMessage.js
+++ sendMessage.js
@@ -1,31 +1,11 @@
-// lambda/sendMessage.js
-const AWS = require("aws-sdk");
-
-const db = new AWS.DynamoDB.DocumentClient();
-
-exports.handler = async (event) => {
-    const message = JSON.parse(event.body).message;
-
-    const allConnections = await db.scan({
-        TableName: "ChatConnections"
-    }).promise();
-
-    const apigw = new AWS.ApiGatewayManagementApi({
-        endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
+// backend/sendMessage.js
+// Simulate sending a message to all connections
+function sendMessage(senderId, message, connections, callback){
+    connections.forEach(conn => {
+        const displayMsg = (conn === senderId ? "Me: " : "User: ") + message;
+        callback(displayMsg);
     });
+}
 
-    const sendToAll = allConnections.Items.map(async ({ connectionId }) => {
-        try {
-            await apigw.postToConnection({
-                ConnectionId: connectionId,
-                Data: message
-            }).promise();
-        } catch (err) {
-            console.log("Failed:", connectionId);
-        }
-    });
-
-    await Promise.all(sendToAll);
-
-    return { statusCode: 200, body: "Message sent" };
-};
+// Export for frontend simulation
+if (typeof module !== 'undefined') module.exports = { sendMessage };
